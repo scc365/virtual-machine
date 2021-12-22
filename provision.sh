91f699a0b617b3b66025a10975543984a9ab8ff3
Will Fantom <willfantom@gmail.com>
2021-12-22 13:04:41 +0000

    Use more recent docker base images 🐳
    Also pull the tutorial repositories during provision
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>

--- provision.sh
+++ provision.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
+set -e
 
-mininet_repository="https://github.com/advanced-networking/mininet"
-mininet_image="ghcr.io/advanced-networking/mininet:latest"
-ryu_repository="https://github.com/advanced-networking/ryu"
-ryu_image="ghcr.io/advanced-networking/ryu:latest"
+MININET_REPO="https://github.com/mininet/mininet"
+MININET_BRANCH="master"
+RYU_VERSION="4.34"
+MININET_IMAGE="ghcr.io/scc365/mininet:latest"
+MN_IMAGE="ghcr.io/scc365/mn:latest"
+PTCP_IMAGE="ghcr.io/scc365/ptcp:latest"
+RYU_IMAGE="ghcr.io/scc365/ryu:latest"
+ME_IMAGE="ghcr.io/scc365/me:latest"
 
+MININET_TUTORIAL="https://github.com/scc365/mininet-tutorial"
+TESTING_TUTORIAL="https://github.com/scc365/testing-tutorial"
+RYU_TUTORIAL="https://github.com/scc365/ryu-tutorial"
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
@@ -14,10 +22,12 @@ function install_deps() {
         apt-transport-https \
         ca-certificates \
         curl \
+        g++ \
         gcc \
         git \
         gnupg-agent \
         hping3 \
+        iperf3 \
         libffi-dev \
         libssl-dev \
         libxml2-dev \
@@ -28,6 +38,7 @@ function install_deps() {
         python3-dev \
         python3-pip \
         software-properties-common \
+        tmux \
         traceroute \
         tshark \
         vim \
@@ -47,22 +58,39 @@ function install_docker() {
 }
 
 function pull_images() {
-    docker pull $mininet_image
-    docker pull $ryu_image
+    docker pull $MININET_IMAGE
+    docker pull $MN_IMAGE
+    docker pull $RYU_IMAGE
+    docker pull $ME_IMAGE
+    docker pull $PTCP_IMAGE
 }
 
 function install_mininet() {
     cd /root
-    git clone $mininet_repository -b lancs mininet
+    git clone $MININET_REPO -b $MININET_BRANCH ./mininet
     cd mininet
-    sh -c "/root/mininet/util/install.sh -fnv"
+    #FIX: use `https://` rather than `git://` to work with scc builds
+    sed -i 's/git:/https:/g' ./utils/install.sh
+    sh -c "./util/install.sh -fnv"
 }
 
 function install_ryu() {
-    cd /root
-    git clone $ryu_repository -b lancs ryu
-    cd ryu
-    pip3 install .
+    #FIX: recent eventlet versions will cause the install to fail
+    pip install eventlet==0.30.2
+    pip install ryu==$RYU_VERSION
+    curl https://raw.githubusercontent.com/scc365/ryu-base-image/main/requirements.txt -o ./requirements.txt
+    pip install -r ./requirements.txt
+}
+
+function add_tutorials() {
+    cd ~
+    git clone $MININET_TUTORIAL
+    git clone $TESTING_TUTORIAL
+    git clone $RYU_TUTORIAL
+}
+
+function set_bashrc_message() {
+    echo "echo ||>  SCC365 - Advanced Networking  <||" >> ~/.bashrc
 }
 
 install_deps
@@ -70,3 +98,5 @@ install_docker
 pull_images
 install_mininet
 install_ryu
+add_tutorials
+set_bashrc_message
